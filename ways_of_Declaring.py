71239deb68fa6532fde9239a4fb83a5dce006a83
Ashrit K <144222044+Ashritom@users.noreply.github.com>
2024-04-17 18:32:51 +0530

    Update and rename ways_of_printing_Declaring.py to ways_of_Declaring.py

--- ways_of_Declaring.py
+++ ways_of_Declaring.py
@@ -1,28 +1,20 @@
+#static and basic method
 a=10
 b=20
 print("The value of a and b are %d and %d " %(a,b))
-
 print("\n")
+
+#Dynamic method
 
 x=int(input("Enter the value of x: "))
 y=int(input("Enter the value of y: "))
 print("The value of x and y are {x} and {y}".format(x=x,y=y))
-
 print("\n")
+
+#Dynamic method and also taking input for more than 1 variable in a single line ( using of map and split function)
 
 u,v=map(int,input("Enter the values of u and v: ").split())
 print("The value of u and v are {} and {}".format(u,v))
-
 print("\n")
 
-i=100
-j=200
-print("The value of i and j are",i, "and",j)
-
-print("\n")
-
-l,m,n = {int(input("Enter the value of l: ")), int(input("Enter the value of m: ")), int(input("Enter the value of n: "))}
-print(f"The value of l and m and n are {l} and {m} and {n}")
-
-print("\n")
 
